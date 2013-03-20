7975adc9897726cef6e8ba1cdc85c7f2c9b67860
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-03-19 22:08:58 -0700

    Small changes

--- lab1/saxpy_ans.py
+++ lab1/saxpy_ans.py
@@ -5,39 +5,41 @@ import math
 from timeit import default_timer as time
 
 @cuda.autojit
-def saxpy(Out, X, Y, Z):
-    "Compute Out = X * Y + Z"
+def saxpy(Out, a, X, Y):
+    "Compute Out = a * X + Y"
     # ------ Exercise --------
     # Complete this kernel
     # threadIdx ---> cuda.threadIdx
     # blockIdx  ---> cuda.blockIdx
     # blockDim  ---> cuda.blockDim
-    i = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
-    Out[i] = X[i] * Y[i] + Z[i]
+    i = cuda.threadIdx.x  + cuda.blockIdx.x * cuda.blockDim.x
+    Out[i] = a * X[i] + Y[i]
 
 def main():
     # Prepare data
     thread_per_block = 512
     block_per_grid = 10
     n = thread_per_block * block_per_grid
-    X = np.random.random(n)
-    Y = np.random.random(n)
-    Z = np.random.random(n)
+    a = 1.2345
+    X = np.random.random(n).astype(np.float32)
+    Y = np.random.random(n).astype(np.float32)
     Out = np.empty_like(X)
 
     # ------ Exercise --------
     # Host->Device
-    # Complete the transfer for Y, Z, and out
+    # Complete the transfer for X, Y, and Out
     dX = cuda.to_device(X)
     dY = cuda.to_device(Y)
-    dZ = cuda.to_device(Z)
-    dOut = cuda.to_device(Out, copy=True)
+    dOut = cuda.to_device(Out, copy=False)
 
     # Kernel launch
-    block = thread_per_block, 1, 1
-    grid = block_per_grid, 1
-    # C: saxpy <<<grid, block>>> (dOut, dX, dY, dZ)
-    saxpy[grid, block](dOut, dX, dY, dZ)
+    blockdim = thread_per_block, 1, 1
+    griddim = block_per_grid, 1
+
+    # ------ Exercise --------
+    # Kernel launch
+    # Complete the kernel launch for saxpy
+    saxpy[griddim, blockdim](dOut, a, dX, dY)
 
     # ------ Exercise --------
     # Device->Host
@@ -47,7 +49,7 @@ def main():
     print('-- Result --')
     print(Out)
     # Verify
-    print("verify: %s" % np.allclose(X * Y + Z, Out))
+    print("verify: %s" % np.allclose(a * X + Y, Out))
 
 if __name__ == '__main__':
     main()
